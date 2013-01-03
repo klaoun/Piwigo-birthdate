19df41566812ad8d041c87b861d6719901c7f92b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-03 23:11:25 +0000

    [birthdate] Add tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@19774 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d fotoğraf';
+$lang['%d photos'] = '%d fotoğraf';
+$lang['Add a birthdate'] = 'Doğum tarihi ekle';
+$lang['Birthdate'] = 'Doğum tarihi';
+$lang['Birthdate removed for %s'] = '%s için doğum tarihi kaldırıldı';
+$lang['Birthdates'] = 'Doğum tarihleri';
+$lang['Delete birthdate for %s'] = '%s için doğum tarihini sil';
+$lang['Edit a birthdate'] = 'Doğum tarihini düzenle';
+$lang['Manage birthdates'] = 'Doğum tarihlerini yönet';
+$lang['Minute'] = 'Dakika';
+$lang['No photo'] = 'Fotoğraf yok';
+$lang['Now'] = 'Şimdi';
+$lang['Select a birthdate'] = 'Bir doğum tarihi seç';
+$lang['Select a tag'] = 'Bir etiket seç';
+$lang['Who?'] = 'Kim?';
+$lang['on this photo %s is %s old'] = '%s bu fotoğrafta %s yaşında';
+?>
