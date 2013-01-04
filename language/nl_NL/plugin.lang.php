064d61c16083aa9bc90c01662e09d3bbf48f720b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-04 08:23:50 +0000

    [birthdate] Add nl_NL, thanks to : yjb
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@19779 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Select a tag'] = 'Selecteer een label';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d fotos';
+$lang['Add a birthdate'] = 'Voeg een verjaardag toe';
+$lang['Birthdate'] = 'Verjaardag';
+$lang['Birthdate removed for %s'] = 'Verdaag verwijderd van %s';
+$lang['Birthdates'] = 'Verjaardagen';
+$lang['Delete birthdate for %s'] = 'Verwijder verjaardag van %s';
+$lang['Edit a birthdate'] = 'Wijzig een verjaardag';
+$lang['Manage birthdates'] = 'Beheeer verjaardagen';
+$lang['Minute'] = 'Minuut';
+$lang['No photo'] = 'Geen foto';
+$lang['Now'] = 'Nu';
+$lang['Select a birthdate'] = 'Kies een verjaardag';
+$lang['Who?'] = 'Wie?';
+$lang['on this photo %s is %s old'] = 'op deze foto is %s %s jaar';
+?>
