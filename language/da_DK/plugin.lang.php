160d1456b998322421e8a9e38e8706b6b7f237d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-04 16:34:47 +0000

    [birthdate] Add da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@19803 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d fotografi';
+$lang['%d photos'] = '%d fotografier';
+$lang['Add a birthdate'] = 'Tilføj en fødselsdato';
+$lang['Birthdate'] = 'Fødselsdato';
+$lang['Birthdate removed for %s'] = 'Fødselsdato fjernet fra %s';
+$lang['Birthdates'] = 'Fødselsdatoer';
+$lang['Delete birthdate for %s'] = 'Slet fødselsdato fra %s';
+$lang['Edit a birthdate'] = 'Rediger en fødselsdato';
+$lang['Manage birthdates'] = 'Håndter fødselsdatoer';
+$lang['Minute'] = 'Minut';
+$lang['No photo'] = 'Intet fotografi';
+$lang['Now'] = 'Nu';
+$lang['Select a birthdate'] = 'Vælg en fødselsdato';
+$lang['Select a tag'] = 'Vælg et tag';
+$lang['Who?'] = 'Hvem?';
+$lang['on this photo %s is %s old'] = 'på dette fotografi er %s %s gammel';
+?>
