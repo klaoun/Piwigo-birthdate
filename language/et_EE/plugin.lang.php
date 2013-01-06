9cf0ffd5eb5117220647f99220bfd4b2472cf77d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-06 17:03:32 +0000

    [birthdate] Add et_EE, thanks to : kersta
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@19913 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Select a tag'] = 'Vali märge';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d fotod';
+$lang['Add a birthdate'] = 'Lisa sünnipäev';
+$lang['Birthdate'] = 'Sünnipäev';
+$lang['Birthdate removed for %s'] = 'Sünnipäev eemaldatud %s';
+$lang['Birthdates'] = 'Sünnipäevad';
+$lang['Delete birthdate for %s'] = 'Kustuta sünnipäevad %s';
+$lang['Edit a birthdate'] = 'Toimeta sünnipäeva';
+$lang['Manage birthdates'] = 'Halda sünnipäevi';
+$lang['Minute'] = 'Minut';
+$lang['No photo'] = 'Pole fotot';
+$lang['Now'] = 'Praegu';
+$lang['Select a birthdate'] = 'Vali sünnipäev';
+$lang['Who?'] = 'Kes?';
+$lang['on this photo %s is %s old'] = '%s on sellel pildil %s aastane';
+?>
