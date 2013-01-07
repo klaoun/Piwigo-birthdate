5985d7ba03ba803e75d69042d7c2daad3eb44e0b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-07 05:40:32 +0000

    [birthdate] Add pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@19935 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a birthdate'] = 'Adicione uma data de aniversário';
+$lang['Birthdate'] = 'Data de Aniversário';
+$lang['Birthdate removed for %s'] = 'Data de Aniversário removida por %s';
+$lang['Birthdates'] = 'Datas de Aniversários';
+$lang['Delete birthdate for %s'] = 'Apague uma data de aniversário por %s';
+$lang['Edit a birthdate'] = 'Edite uma data de aniversário';
+$lang['Manage birthdates'] = 'Gerencie datas de aniversários';
+$lang['Select a birthdate'] = 'Selecione uma data de aniversário';
+$lang['Select a tag'] = 'Selecione uma etiqueta';
+$lang['Who?'] = 'Quem?';
+$lang['on this photo %s is %s old'] = 'nessa foto %s tem %s anos';
+$lang['Minute'] = 'Minuto';
+$lang['No photo'] = 'Nenhuma foto';
+$lang['Now'] = 'Agora';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d fotos';
+?>
