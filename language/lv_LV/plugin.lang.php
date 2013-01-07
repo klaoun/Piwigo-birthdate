8a284b1c4ea4bd03b9ac19b4f9c54e0ad3fdfc6d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-07 17:50:00 +0000

    [birthdate] Add lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@19946 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Select a tag'] = 'Izvēlēties tagu (birku)';
+$lang['on this photo %s is %s old'] = 'šajā foto %s ir %s vecs';
+$lang['Select a birthdate'] = 'Izvēlēties dzimšanas datumu';
+$lang['No photo'] = 'Nav foto';
+$lang['Minute'] = 'Minūte';
+$lang['Manage birthdates'] = 'Pārvaldīt dzimšanas datumus';
+$lang['Edit a birthdate'] = 'Labot dzimšanas datumu';
+$lang['Delete birthdate for %s'] = 'Izdzēst dzimšanas datumu %s';
+$lang['Birthdate removed for %s'] = 'Dzimšanas datums izdzēsts %s';
+$lang['Birthdates'] = 'Dzimšanas datumi';
+$lang['Add a birthdate'] = 'Pievienot dzimšanas datumu';
+$lang['Birthdate'] = 'Dzimšanas datums';
+$lang['%d photos'] = '%d foto';
+$lang['%d photo'] = '%d foto';
+$lang['Now'] = 'Tagad';
+$lang['Who?'] = 'Kas?';
+?>
