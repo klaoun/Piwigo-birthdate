9774eb6eb7381ca07f52a0e44aa32549ae2b6a22
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-10 23:06:51 +0000

    [birthdate] Add zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@20056 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d 相片';
+$lang['%d photos'] = '%d 相片';
+$lang['No photo'] = '无相片';
+$lang['Add a birthdate'] = '添加一个生日';
+$lang['Birthdate'] = '生日';
+$lang['Birthdate removed for %s'] = '%s 的生日已移除';
+$lang['Birthdates'] = '生日';
+$lang['Delete birthdate for %s'] = '移除 %s 的生日';
+$lang['Edit a birthdate'] = '编辑生日';
+$lang['Manage birthdates'] = '生日管理';
+$lang['Minute'] = '分钟';
+$lang['Now'] = '现在';
+$lang['Select a birthdate'] = '选择一个生日';
+$lang['Select a tag'] = '选择一个标签';
+$lang['Who?'] = '谁？';
+$lang['on this photo %s is %s old'] = '本图中的 %s %s 岁了';
+?>
