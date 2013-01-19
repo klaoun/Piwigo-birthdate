0eda5ede5933c8f2427ab453a5f09f9cfbc36bc2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-19 20:47:25 +0000

    [birthdate] Add ro_RO, thanks to : smarmie
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@20268 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d imagine';
+$lang['%d photos'] = '%d imagini';
+$lang['Add a birthdate'] = 'Adaugă o zi de naştere';
+$lang['Birthdate'] = 'Zi de naştere';
+$lang['Birthdate removed for %s'] = 'Ziua de naştere ştearsă pentru %s';
+$lang['Birthdates'] = 'Zile de naştere';
+$lang['Delete birthdate for %s'] = 'Şterge zilele de naştere pentru %s';
+$lang['Edit a birthdate'] = 'Modifică o zi de naştere';
+$lang['Manage birthdates'] = 'Administrează zilele de naştere';
+$lang['Minute'] = 'Minut';
+$lang['No photo'] = 'Nicio imagine';
+$lang['Now'] = 'Acum';
+$lang['Select a birthdate'] = 'Alege o zi de naştere';
+$lang['Select a tag'] = 'Alege o etichetă';
+$lang['Who?'] = 'Cine?';
+$lang['on this photo %s is %s old'] = 'în imaginea %s are %s ani';
+?>
