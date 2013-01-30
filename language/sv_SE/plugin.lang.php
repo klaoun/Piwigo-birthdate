575e352c6f2e6ee550779e70c4b48dd4b3e21b78
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-30 12:48:45 +0000

    [birthdate] Add sv_SE, thanks to : AndersS
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@20471 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['on this photo %s is %s old'] = 'på detta foto är %s %s gammal';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d foton';
+$lang['Add a birthdate'] = 'Lägg till ett födelsedatum';
+$lang['Birthdate'] = 'Födelsedatum';
+$lang['Birthdate removed for %s'] = 'Födelsedatum borttaget för %s';
+$lang['Birthdates'] = 'Födelsedatum';
+$lang['Delete birthdate for %s'] = 'Ta bort födelsedatum för %s';
+$lang['Edit a birthdate'] = 'Ändra ett födelsedatum';
+$lang['Manage birthdates'] = 'Hantera födelsedatum';
+$lang['Minute'] = 'Minut';
+$lang['No photo'] = 'Inget foto';
+$lang['Now'] = 'Nu';
+$lang['Select a birthdate'] = 'Välj ett födelsedatum';
+$lang['Select a tag'] = 'Välj en tagg';
+$lang['Who?'] = 'Vem?';
+?>
