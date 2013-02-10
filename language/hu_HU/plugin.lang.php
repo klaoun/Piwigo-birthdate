f273bef4f7ef3388ef84ffb11b64372a2513f515
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-10 09:12:58 +0000

    [birthdate] Add hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@20640 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['on this photo %s is %s old'] = 'a %s kép kora: %s';
+$lang['Birthdate removed for %s'] = '%s születésnap törölve';
+$lang['Delete birthdate for %s'] = '%s születésnap törlése';
+$lang['Minute'] = 'Perc';
+$lang['Now'] = 'Mai';
+$lang['Select a birthdate'] = 'Születésnap kiválasztása';
+$lang['Select a tag'] = 'Címke kiválasztása';
+$lang['Who?'] = 'Ki?';
+$lang['%d photo'] = '% kép';
+$lang['%d photos'] = '% kép';
+$lang['Add a birthdate'] = 'Születésnap hozzáadása';
+$lang['Birthdate'] = 'Születésnap';
+$lang['Birthdates'] = 'Születésnapok';
+$lang['Edit a birthdate'] = 'Születésnap szerkesztése';
+$lang['Manage birthdates'] = 'Születésnapok kezelése';
+$lang['No photo'] = 'Nincs kép';
+?>
