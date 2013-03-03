05718201861dde38ef5b7fd15ed2a3feb9df6293
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-03 20:09:07 +0000

    [birthdate] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@21155 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Birthdate'] = 'Рожден ден';
+$lang['on this photo %s is %s old'] = 'в тази снимка %s е %s стар';
+$lang['Who?'] = 'Кой?';
+$lang['Select a tag'] = 'Избор на таг';
+$lang['Select a birthdate'] = 'Избор на рожденна дата';
+$lang['Now'] = 'Сега';
+$lang['No photo'] = 'Няма снимка';
+$lang['Minute'] = 'Минути';
+$lang['Manage birthdates'] = 'Промяна на рожденна дата';
+$lang['Edit a birthdate'] = 'Редакция на рожденна дата';
+$lang['Delete birthdate for %s'] = 'Изтрий рождена дата за %s';
+$lang['Birthdates'] = 'Рождени дни';
+$lang['Birthdate removed for %s'] = 'Рожденната дата е премахната от %s';
+$lang['Add a birthdate'] = 'Въведи рожденна дата';
+$lang['%d photos'] = '%d снимки';
+$lang['%d photo'] = '%d снимка';
+?>
