7ee7dc7de1ca5ba440b12978389bb6ffa11b4889
ddtddt <ddtddt@piwigo.org>
2013-03-10 15:57:31 +0000

    [extensions] - birthdate  -Add cs_CZ - Česky – Czech - Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@21440 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['Birthdate'] = 'Datum narození';
+$lang['Birthdates'] = 'Data narození';
+$lang['on this photo %s is %s old'] = 'na této fotce %s je mu/jí %s let';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d fotek';
+$lang['Add a birthdate'] = 'Zadat datum narození';
+$lang['Edit a birthdate'] = 'Editovat datum narození';
+$lang['Manage birthdates'] = 'Správa dat narození';
+$lang['Delete birthdate for %s'] = 'Odstranit datum narození pro %s';
+$lang['Birthdate removed for %s'] = 'Datum narození odstraněno pro %s';
+$lang['Who?'] = 'Kdo to je?';
+$lang['Select a birthdate'] = 'Výběr data narození';
+$lang['Select a tag'] = 'Výběr popisku';
+$lang['No photo'] = 'Žádné foto';
+$lang['Now'] = 'Nyní';
+$lang['Minute'] = 'Minut';
+?>
