81614e2934a37690b443ae49d4129120570b933c
ddtddt <ddtddt@piwigo.org>
2013-03-15 16:53:44 +0000

    [extensions] - birthdate - Add fr_CA - Quebec - Québécois - Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@21580 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['Birthdate'] = 'Date de naissance';
+$lang['Birthdates'] = 'Dates de naissance';
+$lang['on this photo %s is %s old'] = 'sur cette photo %s a %s';
+$lang['%d photo'] = '%d photo';
+$lang['%d photos'] = '%d photos';
+$lang['Add a birthdate'] = 'Ajouter une date de naissance';
+$lang['Edit a birthdate'] = 'Editer une date de naissance';
+$lang['Manage birthdates'] = 'Gérer les dates de naissance';
+$lang['Delete birthdate for %s'] = 'Supprimer la date de naissance de %s';
+$lang['Birthdate removed for %s'] = 'Date de naissance de %s supprimée';
+$lang['Who?'] = 'Qui ?';
+$lang['Select a birthdate'] = 'Sélectionnez une date de naissance';
+$lang['Select a tag'] = 'Sélectionnez un mot clé';
+$lang['No photo'] = 'Aucune photo';
+$lang['Now'] = 'Maintenant';
+$lang['Minute'] = 'Minute';
+?>
