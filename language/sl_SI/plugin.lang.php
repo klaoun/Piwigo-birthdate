3d87ea788bc8ecdc3ac4e04c5b97d279b400cd3f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-15 18:04:42 +0000

    [birthdate] Add sl_SI, thanks to : obucek
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@21586 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d slika';
+$lang['%d photos'] = '%d slik';
+$lang['Add a birthdate'] = 'Dodaj rojstni datum';
+$lang['Birthdate'] = 'Datum rojstva';
+$lang['Birthdate removed for %s'] = 'Rojstni dan umaknjen za %s';
+$lang['Birthdates'] = 'Rojstni datumi';
+$lang['Delete birthdate for %s'] = 'Izbriši datum rojstva';
+$lang['Edit a birthdate'] = 'Uredi datum rojstva';
+$lang['Manage birthdates'] = 'Urejaj rojstne datume';
+$lang['Minute'] = 'Minute';
+$lang['No photo'] = 'Ni slike';
+$lang['Now'] = 'Sedaj';
+$lang['Select a birthdate'] = 'Izberi datum rojstva';
+$lang['Select a tag'] = 'Izberi oznako';
+$lang['Who?'] = 'Kdo?';
+$lang['on this photo %s is %s old'] = 'Na tej sliki %s je star %s';
+?>
