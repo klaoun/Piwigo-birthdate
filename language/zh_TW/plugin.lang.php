868a15cea4c5bb8eabcf421e69f5a858ce23c49c
ddtddt <ddtddt@piwigo.org>
2013-03-15 16:58:06 +0000

    [extensions] - birthdate - Add zh_TW - 中文 (繁體) - Chinese traditional - Thanks to chouyh
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@21581 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['Birthdate'] = '生日'; 
+$lang['Birthdates'] = '生日'; 
+$lang['on this photo %s is %s old'] = '在這張照片上%s是%s歲了'; 
+$lang['%d photo'] = '%d 照片'; 
+$lang['%d photos'] = '%d 照片'; 
+$lang['Add a birthdate'] = '添加生日'; 
+$lang['Edit a birthdate'] = '編輯生日'; 
+$lang['Manage birthdates'] = '管理生日'; 
+$lang['Delete birthdate for %s'] = '刪除%s的生日'; 
+$lang['Birthdate removed for %s'] = '已移除%s的生日'; 
+$lang['Who?'] = '誰?'; 
+$lang['Select a birthdate'] = '選擇一個生日'; 
+$lang['Select a tag'] = '選擇一個標籤'; 
+$lang['No photo'] = '沒有照片'; 
+$lang['Now'] = '現在'; 
+$lang['Minute'] = '分';
+?>
