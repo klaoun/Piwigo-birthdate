bf345829de1a5f27a7d333628216545fa98e60ae
ddtddt <ddtddt@piwigo.org>
2013-03-20 17:59:33 +0000

    [extensions] - birthdate - add vi_VN - Tiếng Việt - Vietnamese thanks to Em Ku
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@21752 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['Birthdate'] = 'Ngày sinh';
+$lang['Birthdates'] = 'Ngày sinh';
+$lang['on this photo %s is %s old'] = 'ở ảnh này thì %s là %s tuổi';
+$lang['%d photo'] = '%d ảnh';
+$lang['%d photos'] = '%d ảnh';
+$lang['Add a birthdate'] = 'Thêm vào ngày sinh';
+$lang['Edit a birthdate'] = 'Chỉnh sửa ngày sinh';
+$lang['Manage birthdates'] = 'Quản lý ngày sinh';
+$lang['Delete birthdate for %s'] = 'Xóa ngày sinh trong %s';
+$lang['Birthdate removed for %s'] = 'Ngày sinh đã bị xóa trong %s';
+$lang['Who?'] = 'Ai?';
+$lang['Select a birthdate'] = 'Chọn một ngày sinh';
+$lang['Select a tag'] = 'Chọn thẻ ảnh';
+$lang['No photo'] = 'Không có ảnh';
+$lang['Now'] = 'Bây giờ';
+$lang['Minute'] = 'Phút';
+
+?>
