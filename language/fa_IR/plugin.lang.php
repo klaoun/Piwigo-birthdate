fa27982f4948c84c8d951d2b1eaecd17a9d6b84f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-21 05:42:10 +0000

    [birthdate] Add fa_IR, thanks to : mnsb13600
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@21754 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Birthdates'] = 'تاریخ تولد';
+$lang['Delete birthdate for %s'] = 'حذف تاریخ تولد';
+$lang['Minute'] = 'دقیقه';
+$lang['on this photo %s is %s old'] = 'در تصاویر قدیمی';
+$lang['Who?'] = 'کیه';
+$lang['Select a tag'] = 'انتخاب تگ';
+$lang['Select a birthdate'] = 'انتخاب تاریخ تولد';
+$lang['Now'] = 'اکنون';
+$lang['No photo'] = 'بدون تصویر';
+$lang['Manage birthdates'] = 'مدیریت تاریخ تولد';
+$lang['Edit a birthdate'] = 'ویرایش تاریخ تولد';
+$lang['Birthdate removed for %s'] = 'حذف تاریخ تولد';
+$lang['Birthdate'] = 'تاریخ تولد';
+$lang['%d photos'] = 'تصاویر';
+$lang['Add a birthdate'] = 'افزودن تاریخ تولد';
+$lang['%d photo'] = 'عکس';
+?>
