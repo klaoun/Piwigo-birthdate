d08e810521e686e09944632e0e1c954fd8fee92a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-20 17:39:20 +0000

    [birthdate] Add lt_LT, thanks to : vilisas
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@23400 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lt_LT/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d nuotrauka';
+$lang['%d photos'] = '%d nuotraukų(-os)';
+$lang['Add a birthdate'] = 'Pridėti gimtadienį';
+$lang['Birthdate'] = 'Gimtadienis';
+$lang['Birthdate removed for %s'] = '%s gimtadienis pašalintas';
+$lang['Birthdates'] = 'Gimtadieniai';
+$lang['Delete birthdate for %s'] = 'Pašalinti %s gimtadienį';
+$lang['Edit a birthdate'] = 'Redaguoti gimtadienį';
+$lang['Manage birthdates'] = 'Tvarkyti gimtadienius';
+$lang['Minute'] = 'Minutė';
+$lang['No photo'] = 'Nėra nuotraukos';
+$lang['Now'] = 'Dabar';
+$lang['Select a birthdate'] = 'Pasirinkite gimtadienį';
+$lang['Select a tag'] = 'Pasirinkite žymą';
+$lang['Who?'] = 'Kas ?';
+$lang['on this photo %s is %s old'] = 'šioje nuotraukoje %s amžius yra: %s';
+?>
