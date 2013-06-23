4f0e52edd08daf9ff1035d367608c5822d4a07af
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-23 10:01:31 +0000

    [birthdate] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@23461 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Delete birthdate for %s'] = 'Forigi naskiĝdaton por %s';
+$lang['Edit a birthdate'] = 'Redakti naskiĝdaton';
+$lang['Manage birthdates'] = 'Administri naskiĝdatojn';
+$lang['Minute'] = 'Minuto';
+$lang['No photo'] = 'Neniu foto';
+$lang['Now'] = 'Nun';
+$lang['Select a birthdate'] = 'Elekti naskiĝdaton';
+$lang['Select a tag'] = 'Elekti etikedon';
+$lang['Who?'] = 'Kiu?';
+$lang['on this photo %s is %s old'] = 'en tiu foto %s estas %s-jaraĝa';
+$lang['Birthdates'] = 'Naskiĝdatoj';
+$lang['Birthdate removed for %s'] = 'Naskiĝdato forigita por %s';
+$lang['Birthdate'] = 'Naskiĝdato';
+$lang['Add a birthdate'] = 'Aldoni naskiĝdaton';
+$lang['%d photos'] = '%d fotoj';
+$lang['%d photo'] = '%d foto';
+?>
