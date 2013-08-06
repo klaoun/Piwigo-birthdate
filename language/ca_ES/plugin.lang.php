2dabc76742534223eebd95cd4d1cf656c39acf52
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-06 07:50:56 +0000

    [birthdate] Add ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@24121 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d fotos';
+$lang['Add a birthdate'] = 'Afegeix un aniversari';
+$lang['Birthdate'] = 'Aniversari';
+$lang['Birthdate removed for %s'] = 'Aniversari eliminat de %s';
+$lang['Birthdates'] = 'Aniversaris';
+$lang['Delete birthdate for %s'] = 'Elimina l\'aniversari de %s';
+$lang['Edit a birthdate'] = 'Edita un aniversari';
+$lang['Manage birthdates'] = 'Gestiona els aniversaris';
+$lang['Minute'] = 'Minut';
+$lang['No photo'] = 'Sense foto';
+$lang['Now'] = 'Ara';
+$lang['Select a birthdate'] = 'Indiqueu un aniversari';
+$lang['Select a tag'] = 'Indiqueu una etiqueta';
+$lang['Who?'] = 'Qui?';
+$lang['on this photo %s is %s old'] = 'aquesta foto %s és %s vella';
+?>
