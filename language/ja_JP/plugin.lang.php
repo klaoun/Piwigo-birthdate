d2a90b841c3b13f04c286362792f398200b41c51
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-26 22:20:04 +0000

    [birthdate] Add ja_JP, thanks to ma2tak
    git-svn-id: http://piwigo.org/svn/extensions/birthdate@30620 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['on this photo %s is %s old'] = 'この写真は、%s が %s歳のものです';
+$lang['%d photo'] = '%d 枚の写真';
+$lang['%d photos'] = '%d 枚の写真';
+$lang['Add a birthdate'] = '誕生日を追加';
+$lang['Birthdate'] = '誕生日';
+$lang['Birthdate removed for %s'] = 'タグ %s から誕生日を削除';
+$lang['Birthdates'] = '誕生日';
+$lang['Delete birthdate for %s'] = 'タグ %s から誕生日を削除';
+$lang['Edit a birthdate'] = '誕生日を編集';
+$lang['Manage birthdates'] = '誕生日の管理';
+$lang['Minute'] = '分';
+$lang['No photo'] = '写真なし';
+$lang['Now'] = '現在';
+$lang['Select a birthdate'] = '誕生日の選択';
+$lang['Select a tag'] = 'タグを選択';
+$lang['Who?'] = '誰？';
