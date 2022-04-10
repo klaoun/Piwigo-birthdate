e4b4d824816e173311c005023c9cdfaf2ee984a0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-10 23:34:12 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d mynd';
+$lang['%d photos'] = '%d myndir';
+$lang['Add a birthdate'] = 'Bæta við afmælisdegi';
+$lang['Birthdate'] = 'Afmælisdagur';
+$lang['Birthdate removed for %s'] = 'Afmælisdagur fjarlægður hjá %s';
+$lang['Birthdates'] = 'Afmælisdagar';
+$lang['Delete birthdate for %s'] = 'Eyða afmælisdegi fyrir %s';
+$lang['Edit a birthdate'] = 'Breyta afmælisdegi';
+$lang['Manage birthdates'] = 'Stjórna afmælisdögum';
+$lang['Minute'] = 'Mínútur';
+$lang['No photo'] = 'Engin mynd';
+$lang['Now'] = 'Núna';
+$lang['Select a birthdate'] = 'Velja afmælisdag';
+$lang['Select a tag'] = 'Velja merki';
+$lang['Who?'] = 'Hver?';
+$lang['on this photo %s is %s old'] = 'í þessari mynd %s er %s ára';
