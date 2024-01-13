329a505edf9c7aba52544996bb3fddf3dfdc050a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-13 15:07:22 +0100

    Update fr_FR, thanks to plg

--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -5,7 +5,7 @@ $lang['on this photo %s is %s old'] = 'sur cette photo %s a %s';
 $lang['%d photo'] = '%d photo';
 $lang['%d photos'] = '%d photos';
 $lang['Add a birthdate'] = 'Ajouter une date de naissance';
-$lang['Edit a birthdate'] = 'Editer une date de naissance';
+$lang['Edit a birthdate'] = 'Éditer une date de naissance';
 $lang['Manage birthdates'] = 'Gérer les dates de naissance';
 $lang['Delete birthdate for %s'] = 'Supprimer la date de naissance de %s';
 $lang['Birthdate removed for %s'] = 'Date de naissance de %s supprimée';
@@ -15,4 +15,3 @@ $lang['Select a tag'] = 'Sélectionnez un tag';
 $lang['No photo'] = 'Aucune photo';
 $lang['Now'] = 'Maintenant';
 $lang['Minute'] = 'Minute';
-?>
